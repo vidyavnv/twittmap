28b5ad72296e137015e3e5d1da3c60c6aec45965
vidyavnv <vidya.vnv@gmail.com>
2016-10-23 16:01:52 -0400

    change app name to application: aws eb error resolve

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask
-app = Flask(__name__)
+application = Flask(__name__)  # Change assignment here
 
-@app.route("/")
-def main():
-	return "Welcome!"
+@application.route("/")        # Change your route statements
+def hello():         
+    return "Hello World!"
 
-if __name__ == "__main__":
-	app.run()
+if __name__ == "__main__":         
+    application.run()          # Change all other references to 'app'
