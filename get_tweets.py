a1134c2267f35884429b844dee55a9999a2db9b6
vidyavnv <vidya.vnv@gmail.com>
2016-10-24 13:08:53 -0400

    Add elastic search functions

--- get_tweets.py
+++ get_tweets.py
@@ -1,25 +1,37 @@
 import sys
 import json
+from datetime import datetime
 
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream, API
 
 from config import access_token, access_token_secret, \
-                   consumer_secret, consumer_key
+                   consumer_secret, consumer_key, \
+                   ES_INDEX, ES_TYPE
+from settings import es, gmaps
 
 
 class StreamListener(StreamListener):
     def on_data(self, data):
-        print data
-        # parse data based on 
         tweet = json.loads(data)
-        if tweet.get('lang') == 'en' and tweet.get('coordinates') is not None:
-                print tweet['coordinates']
+        if tweet['lang'] == 'en' and tweet['user'].get('location') is not None:
+            location = tweet['user'].get('location')
+            tweet_id = str(tweet['id'])
+            geocode_result = gmaps.geocode(location)
+            raw_tweet = {
+                'user': tweet['user']['screen_name'],
+                'text': tweet['text'],
+                'location': tweet['user']['location'],
+                'coordinates': geocode_result, 
+                'time': tweet['created_at']
+            }
+            es.index(index=ES_INDEX, doc_type=ES_TYPE, id=tweet_id, body=raw_tweet)
+            return True
 
     def on_error(self, status):
         print status
-        sys.exit()
+        # sys.exit()
 
 
 if __name__ == '__main__':
@@ -31,4 +43,4 @@ if __name__ == '__main__':
     stream = Stream(api.auth, listener=listener)
 
     while True:
-        stream.filter(track=['kevin hogan'])
+        stream.filter(track=['sports', 'music', 'hillary'])
