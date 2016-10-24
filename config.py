df7bec27b14e9e08f1dce65139b84300601e84b9
vidyavnv <vidya.vnv@gmail.com>
2016-10-24 13:07:54 -0400

    Add elastic search params and google maps params

--- config.py
+++ config.py
@@ -2,3 +2,10 @@ access_token = "1191067658-0klncDjL2UMBI8QrLMItOMdQ263qKobnwkUWXoS"
 access_token_secret = "2sW3bdKIeXqWzBL6s8sdhTRotkPQL6SZ24sVT0YW3TS7X"
 consumer_key = "dfzRJWh6Av53w6J8l9AY58xeh"
 consumer_secret = "TwzHxUeeVbD35cVhaMfg0bofRM9gljS8ncQJuEPo8b2p4QCUe9"
+
+ES_PORT  = "9200"
+ES_HOST = "localhost" 
+ES_INDEX = 'twittmap'
+ES_TYPE = 'tweets'
+
+GMAP_KEY = 'AIzaSyCe3_nUSzR3ES6Im0nTGyIjen_imVOLpOs'
